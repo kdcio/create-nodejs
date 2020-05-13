c6cfac38592867f2c9179849fddfcb7e8f5c4cd1
Ian Dela Cruz <iandc76@gmail.com>
2020-05-13 10:29:00 +0800

    Add bin

--- bin/kdc-create-nodejs.js
+++ bin/kdc-create-nodejs.js
@@ -1,4 +1,13 @@
 #!/usr/bin/env node
 
-var run = require('../lib');
-run();
+const { program } = require('commander');
+
+program
+  .name('npx @kdcsoftware/create-nodejs')
+  .usage('<package-name>')
+  .arguments('<package-name>', 'Name of your package')
+  .action(function (packageName) {
+    // console.log(packageName);
+    require('../lib')({ packageName });
+  });
+program.parse(process.argv);
