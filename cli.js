85eedf1b03d0581ebcce77dc5efe9dff3beaa4fc
Ian Dela Cruz <iandc76@gmail.com>
2020-05-13 13:55:07 +0800

    Add cli options

--- cli.js
+++ cli.js
@@ -1,15 +1,65 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-console */
+const chalk = require('chalk');
+const envinfo = require('envinfo');
 const { program } = require('commander');
 const run = require('./lib/index').default;
 
-program
-  .name('npx @kdcsoftware/create-nodejs')
-  .usage('<package-name>')
-  .arguments('<package-name>', 'Name of your package')
-  .action(function (packageName) {
-    // console.log(packageName);
-    run({ packageName });
-  });
+const packageJson = require('./package.json');
 
-program.parse(process.argv);
+let packageName;
+
+program
+  .version(packageJson.version)
+  .name('npx @kdcsoftware/create-nodejs')
+  .usage(`${chalk.green('<package-name>')}`)
+  .arguments('<package-name>')
+  .option('--info', 'print environment debug info')
+  .action((name) => {
+    packageName = name;
+  })
+  .parse(process.argv);
+
+if (program.info) {
+  console.log(chalk.bold('\nEnvironment Info:'));
+  console.log(
+    `\n  current version of ${packageJson.name}: ${packageJson.version}`
+  );
+  console.log(`  running from ${__dirname}`);
+  return envinfo
+    .run(
+      {
+        System: ['OS', 'CPU'],
+        Binaries: ['Node', 'npm', 'Yarn'],
+        Browsers: ['Chrome', 'Edge', 'Internet Explorer', 'Firefox', 'Safari'],
+        npmGlobalPackages: ['@kdcsoftware/create-nodejs'],
+      },
+      {
+        duplicates: true,
+        showNotFound: true,
+      }
+    )
+    .then(console.log);
+}
+
+if (typeof packageName === 'undefined') {
+  console.error('Please specify the project directory:');
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`
+  );
+  console.log();
+  console.log('For example:');
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green('my-awesome-package')}`
+  );
+  console.log();
+  console.log(
+    `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
+  );
+  return process.exit(1);
+}
+
+run({ packageName });
+
+return process.exit(0);
